da871014bddd78788f293c8b594ed26f29ec7e32
David Bushell <david@dbushell.com>
2012-06-06 12:05:27 +0100

    fix readyState in IE9

--- socialite.js
+++ socialite.js
@@ -12,7 +12,7 @@ window.Socialite = (function(window, document, undefined)
         instances = [ ],
         networks  = { },
         widgets   = { },
-        rstate    = /^($|complete)/,
+        rstate    = /^($|loaded|complete)/,
         euc       = window.encodeURIComponent;
 
     var socialite = {
@@ -287,8 +287,9 @@ window.Socialite = (function(window, document, undefined)
                 network = networks[network];
             }
             for (var i = 0; i < instances.length; i++) {
-                if (instances[i].init && instances[i].widget.network === network) {
-                    socialite.activateInstance(instances[i]);
+                var instance = instances[i];
+                if (instance.init && instance.widget.network === network) {
+                    socialite.activateInstance(instance);
                 }
             }
         },
