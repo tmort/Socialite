73d7d945110fcf7a2a1f81a517755d9f06c17a60
Ilya Grigorik <ilya@igvita.com>
2012-06-10 10:26:14 -0700

    use active to render HN widgets - https://github.com/igrigorik/hackernews-button/commit/577cf9ca919df6da8a4a92c8e1a39bd9baa5e58c

--- extensions/socialite.hackernews.js
+++ extensions/socialite.hackernews.js
@@ -1,5 +1,7 @@
+//
 // Hacker News
 // https://github.com/igrigorik/hackernews-button
+//
 (function(window, document, Socialite, undefined)
 {
 
@@ -9,14 +11,20 @@
         }
     });
 
-    var hackernewsInit = function(instance)
-    {
+    var hackernewsInit = function(instance) {
         var el = document.createElement('a');
         el.className = 'hn-share-button';
         Socialite.copyDataAttributes(instance.el, el);
         instance.el.appendChild(el);
     };
 
-    Socialite.widget('hackernews', 'share', { init: hackernewsInit });
+    Socialite.widget('hackernews', 'share', {
+        init: hackernewsInit,
+        activate: function(instance) {
+            if (window.HN) {
+                window.HN.render(instance.el);
+            }
+        }
+    });
 
 })(window, window.document, window.Socialite);
